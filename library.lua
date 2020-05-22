1361b72dc66f73a34efb960ba3e4bfd4c46ed4ee
HaroldLikesLua <65769964+HaroldLikesLua@users.noreply.github.com>
2020-05-22 14:43:57 +0200

    Create library.lua

---
+++ library.lua
@@ -0,0 +1,58 @@
+local library = {}
+
+local clock = os.clock
+function sleep(n)  -- seconds
+    local t0 = clock()
+    while clock() - t0 <= n do end
+end
+
+function library:encrypt(X)
+    local function ToByte(str)
+        local Total = "::"
+        for i = 1, str:len() do
+            local Byte = string.byte(str, i)
+            Total = Total .. (Byte /5) .. "::"
+            sleep(0)
+        end
+        return Total
+    end
+    return (ToByte(X))
+end
+
+function library:decrypt(X)
+    function split(pString, pPattern)
+        local Table = {}
+        local fpat = "(.-)" .. pPattern
+        local last_end = 1
+        local s, e, cap = pString:find(fpat, 1)
+        while s do
+            if s ~= 1 or cap ~= "" then
+                table.insert(Table,cap)
+            end
+            last_end = e+1
+            s, e, cap = pString:find(fpat, last_end)
+        end
+        if last_end <= #pString then
+            cap = pString:sub(last_end)
+            table.insert(Table, cap)
+        end
+        return Table
+    end
+
+    local Total = ""
+
+    local Splitted = split(X, "::")
+    for i = 1, #Splitted do
+        local Byte = tonumber(Splitted[i])
+        Byte = Byte * 5
+        Total = Total .. string.char(Byte)
+        sleep(0)
+    end
+
+    return(Total)
+    -- local Byte = string.byte(str, i) * 0x5
+    -- Total = Total .. (Byte * (Byte * 2)) .. "::"
+
+end
+
+return library
